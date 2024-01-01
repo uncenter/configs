8e75f933e66107ea9ae02501dd34807a1e662fb4
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-01 14:44:11 -0500

    fix: use eslint flatcompat for typescript-eslint

--- packages/eslint/core/typescript.js
+++ packages/eslint/core/typescript.js
@@ -1,29 +1,24 @@
-/* Parsers */
-import * as typescriptParser from '@typescript-eslint/parser';
-
-/* Plugins */
-import typescript from '@typescript-eslint/eslint-plugin';
-
 /* Configs */
 import base from './base.js';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	base,
-	typescript.configs['recommended'],
-	{
-		files: ['**/*.{ts,tsx}'],
-		languageOptions: {
-			parser: typescriptParser,
-			parserOptions: {
-				project: 'tsconfig.json',
-			},
-		},
-		plugins: {
-			typescriptEslint: typescript,
+	compat.config({
+		extends: ['plugin:@typescript-eslint/recommended'],
+		plugins: ['@typescript-eslint'],
+		parser: '@typescript-eslint/parser',
+		parserOptions: {
+			project: 'tsconfig.json',
 		},
 		rules: {
 			'@typescript-eslint/consistent-type-imports': 'error',
 		},
-	},
+	}),
 ];
