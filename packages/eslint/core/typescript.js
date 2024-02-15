52133a64e728d660ffb4ad84977ba1b3bca38681
uncenter <47499684+uncenter@users.noreply.github.com>
2024-02-15 07:14:08 -0500

    refactor(eslint-config): switch to typescript-eslint flat config from compat

--- packages/eslint/core/typescript.js
+++ packages/eslint/core/typescript.js
@@ -1,24 +1,23 @@
 /* Configs */
 import base from './base.js';
 
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-const compat = new FlatCompat({ resolvePluginsRelativeTo: __dirname });
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	base,
-	compat.config({
-		extends: ['plugin:@typescript-eslint/recommended'],
-		plugins: ['@typescript-eslint'],
-		parser: '@typescript-eslint/parser',
-		parserOptions: {
-			project: 'tsconfig.json',
+	{
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+		},
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: true,
+			},
 		},
 		rules: {
 			'@typescript-eslint/consistent-type-imports': 'error',
 		},
-	}),
+	},
 ];
