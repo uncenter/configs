4b0303d84b799f07bcd72346a3e91e27b66acf1f
uncenter <47499684+uncenter@users.noreply.github.com>
2024-02-15 21:20:36 -0500

    feat(prettier-config): export definePrettierConfig compose function

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,10 +1,13 @@
-export default {
-	useTabs: true,
-	printWidth: 80,
-	trailingComma: 'all',
-	semi: true,
-	singleQuote: true,
-	quoteProps: 'consistent',
+const defaultOptions = {
+	options: {
+		useTabs: true,
+		printWidth: 80,
+		trailingComma: 'all',
+		semi: true,
+		singleQuote: true,
+		quoteProps: 'consistent',
+	},
+	ignores: ['pnpm-lock.yaml'],
 	overrides: [
 		{
 			// Use double quotes, tabs, and two spaces in YAML files (mostly .github/).
@@ -15,13 +18,26 @@ export default {
 				useTabs: false,
 			},
 		},
-		{
-			// Disables formatting of pnpm-lock.yaml (https://github.com/fzf404/prettier-config/).
-			files: 'pnpm-lock.yaml',
-			options: {
-				// Requires a "pragma" comment at the top of file to enable formatting.
-				requirePragma: true,
-			},
-		},
 	],
 };
+
+export function definePrettierConfig(opts = {}) {
+	const options = {
+		options: { ...defaultOptions.options, ...opts.options },
+		ignores: [...defaultOptions.ignores, ...(opts.ignores || [])],
+		overrides: [...defaultOptions.overrides, ...(opts.overrides || [])],
+	};
+
+	options.overrides.push({
+		files: options.ignores,
+		options: {
+			// Requires a "pragma" comment at the top of file to enable formatting.
+			requirePragma: true,
+		},
+	});
+
+	return {
+		...options.options,
+		overrides: options.overrides,
+	};
+}
